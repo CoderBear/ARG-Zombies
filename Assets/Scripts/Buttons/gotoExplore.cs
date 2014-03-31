124dcb43ae9759f712ff84b1251fdaaed73d8b03
KaradurZero <Karadur_Zero@yahoo.com>
2014-03-30 18:46:47 -0700

    button additions and script
    duplicated and added buttons for shop and healing alongside empty
    building in offline map scene.
    added tags Shop, Healing, Empty Building to project and tagged the three
    buttons appropriately.
    added script to new buttons.
    updated gotoexplore script to take into account new tags. will now load
    level depending upon which tag the button has (will need other levels
    made for that).
    tested and ensured buttons work. shop, healing, emtpy building buttons
    will print a message to the debug console when clicked.

--- Assets/Scripts/Buttons/gotoExplore.cs
+++ Assets/Scripts/Buttons/gotoExplore.cs
@@ -13,6 +13,26 @@ public class gotoExplore : MonoBehaviour {
 
 	void OnClick() {
 		// Future: will read the GO tag and load the correct building type
-		Application.LoadLevel ("gameBuilding01");
+		//string tag = this.tag
+		string tag = this.tag;
+		switch(tag){
+		case "Shop":
+			//load shop
+			Debug.Log("level Shop loaded from tag");
+			break;
+		case "Healing":
+			//load healing level
+			Debug.Log("Level Healing loaded from tag");
+			break;
+		case "Empty Building":
+			//load random empty building
+			Debug.Log("Level Empty Building loaded from tag");
+			Application.LoadLevel ("gameBuilding01");//can be changed later if there are different empty buildings to go through.
+			break;
+		default:
+			Debug.Log("Level load error: Tag "+tag+" non case match. default used");
+			Application.LoadLevel ("gameBuilding01");
+			break;
+		}
 	}
 }
