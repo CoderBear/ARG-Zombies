3ddbe4691d4e281774b904ae92b9e81007adb20b
KaradurZero <Karadur_Zero@yahoo.com>
2014-04-09 18:20:03 -0700

    minor updates to offline button spawner

--- Assets/Scripts/Buttons/offlineButtonSpawn.cs
+++ Assets/Scripts/Buttons/offlineButtonSpawn.cs
@@ -7,32 +7,33 @@ public class offlineButtonSpawn : MonoBehaviour {
 	public GameObject m_healing;
 	public GameObject m_shop;
 	public GameObject m_NGUIMissionparent;
+	float randomRange_x = 300;
+	float randomRange_y = 225;
 
 	GameObject [] m_buildings;
 	int maxBuildings = 5;
-	//bool ranOnce = false;
-	// Use this for initialization
-	void Start () {
-		m_buildings = new GameObject [maxBuildings];
 
+	void loadRandomBuildings() {
 		//testing purposes. load buildings manually into array.
-		Vector3 a_position = new Vector3(Random.Range(-300, 300), Random.Range(-225, 225), 0);
-		//Debug.Log("a_position's random X: "+ a_position.x +" random Y: "+ a_position.y);
 		m_buildings[0] = NGUITools.AddChild(m_NGUIMissionparent, m_emptyBuilding);//(GameObject)Instantiate(m_emptyBuilding, Vector3.zero, Quaternion.identity);
-		//a_position = new Vector3(Random.Range(-300, 300), Random.Range(-225, 225), 0);
-		//m_buildings[1] = (GameObject)Instantiate(m_healing, a_position, Quaternion.identity);
+		Vector3 a_position = new Vector3(Random.Range(-randomRange_x, randomRange_x), Random.Range(-randomRange_y, randomRange_y), 0);
 		m_buildings[0].transform.localPosition = a_position;
+		
+		for(int range = 1; range < maxBuildings; ++range) {
+			switch( (int)Random.value%3) {
+				
+			}
+		}
 	}
 
-//	void runOnce() {
-//		m_buildings[0].transform.position = new Vector3(Random.Range(-300, 300), Random.Range(-225, 225), 0);
-//
-//		ranOnce = true;
-//	}
+	void Start () {
+		m_buildings = new GameObject [maxBuildings];
+	}
+
+
 
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log("building 0 transform X: "+ m_buildings[0].transform.position.x +" Y: "+ m_buildings[0].transform.position.y);
-		//if(!ranOnce) {runOnce();}
+
 	}
 }
