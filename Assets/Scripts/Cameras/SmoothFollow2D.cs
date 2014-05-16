05f81edb5803aa8020fda8790d03ca59123969bc
Joe Ortega <joeortega79@gmail.com>
2014-05-16 10:44:25 -0700

    0.0.24a
    Temp Commit 0.0.25 will make sure everything is up to date.

---
+++ Assets/Scripts/Cameras/SmoothFollow2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2D : MonoBehaviour {
+
+	[SerializeField] private Transform target;
+	[SerializeField] private float smoothTime = 0.3f;
+
+	private tk2dCamera tk2DCamera;
+	private Transform thisTransform;
+	private Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+		tk2DCamera = GetComponent<tk2dCamera> ();
+		thisTransform = tk2DCamera.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.SmoothDamp (thisTransform.position.x, target.position.x, ref velocity.x, smoothTime);
+		float y = Mathf.SmoothDamp (thisTransform.position.y, target.position.y, ref velocity.y, smoothTime);
+
+		thisTransform.position.Set (x, y, thisTransform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private float speed = 5f;
+
+	[SerializeField] tk2dSprite sprite;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow)) {
+			float y = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+			sprite.transform.Translate (0f, y, 0f);
+		}
+		else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.RightArrow)) {
+			float x = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+			sprite.transform.Translate (x, 0, 0);
+		}
+	}
+}
